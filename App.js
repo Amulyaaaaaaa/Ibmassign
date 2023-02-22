457e4452d80bd4126ccf17425ffedc7036a2e8fa
Amulyaaaaaa <123067234+Amulyaaaaaaa@users.noreply.github.com>
2023-02-22 16:14:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import  ToDoItem from "./TodoItem"
+function App(){
+    return(
+    <div>
+       
+       <ToDoItem/>
+       <ToDoItem/>
+       <ToDoItem/>
+       <ToDoItem/>
+        
+
+    </div>
+
+)
+}
+export default App
